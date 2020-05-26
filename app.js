93033ceec3a3c76a4b49451f4f4fe74d1d7adab3
deepak <deepak@brownveg>
2020-05-26 06:53:59 -0700

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const order = require('./product')
+const app= express()
+
+const port= process.env.PORT || 3000
+
+
+app.use(express.json())
+
+
+app.post('/webhook',(req, res) =>{
+
+ 
+ product(req.body.queryResult.parameters.orderno, (error,data) => {
+  if (error) {
+      return res.send({ error })
+  }
+      res.send({
+          
+          fulfillmentMessages:[{"text":{"text":[data]}}]
+          
+       
+      })
+  })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' +port)
+})
---
+++ product.js
@@ -0,0 +1,31 @@
+const request = require('request')
+
+const product = (sku,callback) =>{
+    const url ='https://app.ecwid.com/api/v3/16838918/products?sku='+sku+'&token=secret_1pudxM5Phxy1WwhJWX2cqPRvXV68sc3X'
+request({url: url, json:true},(error, response) => {
+    if(response.body.total==0){
+        callback('Product is not available. ',undefined)
+            
+        }  
+
+        else if(response.body.errorMessage){
+
+            callback('Products is not available try another products',undefined)
+        
+        }
+        else{
+            callback(undefined, {
+                
+                
+                Product: response.body.items[0].name,
+               
+                price: response.body.items[0].price
+            
+            })
+       
+
+        }
+    })
+
+}
+ module.exports= product
